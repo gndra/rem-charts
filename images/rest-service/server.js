ee004710ca67ada558a7111e78a50715b98d2e85
Noe Chavez <noechavez0001@gmail.com>
2021-11-28 19:28:49 -0600

    fix: add extra path

--- images/rest-service/server.js
+++ images/rest-service/server.js
@@ -9,7 +9,8 @@ const router = (req) => {
   if (process.env.PLATFORM_SERVE) {
     const user = req.headers['x-user']
     if (!user) return process.env.PROXY_URL
-    return `https://${user}.${process.env.PLATFORM_SERVE}.rem.tools`
+    const extraPath = process.env.EXTRA_PATH || ''
+    return `https://${user}-${process.env.PLATFORM_SERVE}.rem.tools${extraPath}`
   }
   return process.env.PROXY_URL
 }
